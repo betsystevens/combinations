4d129a67fb4cc4009c3314ef271619ec0f312780
raebaxter <betsystevens@comcast.net>
2019-11-11 19:05:34 -0600

    initial commit

---
+++ facts.js
@@ -0,0 +1,111 @@
+/**
+ *  Permutations & Combinations
+ */
+
+/**
+ * factorial - recursive factorial function
+ * @param     n     positive integer or zero
+ * @return    n!
+ */
+const factorial = (n) => {
+  if (n === 0) {
+    return 1;
+  }
+  else {
+    return n * factorial(n-1);
+  }
+}
+/**
+ * getFactorial - check input, call factorial if input is valid 
+ * @param     n     positive integer
+ * @return    n!    if valid input
+ *            -1    if invalid input
+ */
+const getFactorial = (n) => {
+  if (isNatural(n)){
+    return factorial(+n); 
+  } else {
+    return -1; 
+  }
+}
+/**
+ * getPartialPerm - check input, call partialPerm if input is valid
+ * @param     n     positive integer or 0
+ * @param     k     positive integer or 0
+ * @return    partial permutation if input is valid
+ *            -1    if input is invalid
+ */
+const getPartialPerm = (n, k) => {
+  if ( (isNatural(n)) && (isNatural(k)) ){
+    return partialPerm(+n, +k); 
+  } else {
+    return -1;
+  }
+}
+/**
+ *  partialPerm - Partial Permutation, recursive
+ *  @param    n     positive integer
+ *  @param    k     positive integer
+ *  @return   k-permutations of n
+ * 
+ *  k-permutations of n 
+ *  Order matters 
+ *  Permutation but only with k chosen, ie. partial
+ * 
+ *  Example problem: you have 6 people to choose from and 4 chairs
+ *  How many ways can you seat 4 of the people?
+ *  n = 6, k = 4
+ *  Answer: 6*5*4*3 = 360
+ *  Edge cases: (0,0), (1,0), (0,1) => 1
+ *              (4,6) => (4,4)  // k > n => (n,n)
+ */
+const partialPerm = (n, k) => {
+  if ((k === 0) || (n === 0) || (n === 1)) {
+    return 1;
+  }
+  else {
+    return n * (partialPerm (n-1, k-1));
+  }
+}
+/**
+ *  combination 
+ *  n choose k items 
+ *  @param    n     positive integer
+ *  @param    k     positive integer
+ *  @return   n choose k  
+ * 
+ *  binomial coefficient
+ *  defined for k <= n
+ *  n! / (n - k)! k!
+ * 
+ */
+const combination = (n, k) => {
+  if ( (isNatural(n)) && (isNatural(k)) && (k <= n) ){
+    let nFact = getFactorial(n);
+    let kFact = getFactorial(k);
+    let xFact = getFactorial(n-k);
+    return (nFact/(xFact * kFact));
+  } else {
+    return -1;
+  }
+}
+
+/**
+ *  isNatural - verify x is natural number, ie, 0 or positive integers
+ *  @param    x       mixed   
+ *  @return   true    x is natural number
+ *            false   x is not a natural number 
+ */
+const isNatural = (x) => {
+  n = +x;
+  if (n < 0) return false;
+  if (isNaN(n)) return false;
+  if (!Number.isInteger(n)) return false;
+  return true;
+}
+
+module.exports = {
+  getFactorial: getFactorial,
+  getPartialPerm: getPartialPerm,
+  combination:  combination
+}
---
+++ test.js
@@ -0,0 +1,128 @@
+const assert = require('assert');
+const facts = require('./facts.js');
+
+// tests for getFactorial()
+
+it('getFactorial(0) should correctly calculate 0!', () => {
+  assert.equal(facts.getFactorial(0), 1);
+});
+
+it('getFactorial(3) should correctly calculate 3!', () => {
+  assert.equal(facts.getFactorial(3), 6);
+});
+
+it('getFactorial("4") should correctly calculate 3!', () => {
+  assert.equal(facts.getFactorial("4"), 24);
+});
+
+it('getFactorial(-1) should detect invalid number input', () => {
+  assert.equal(facts.getFactorial(-1), -1);
+});
+
+it('getFactorial(2.3) should detect invalid real number', () => {
+  assert.equal(facts.getFactorial(2.3), -1);
+});
+
+it('getFactorial("six") should detect invalid string input', () => {
+  assert.equal(facts.getFactorial("six"), -1);
+});
+
+it('getFactorial("foo") should detect invalid string input', () => {
+  assert.equal(facts.getFactorial("foo"), -1);
+});
+
+// tests for partial permutations
+
+it('should correctly calculate getPartialPerm(0, 0)', () => {
+  assert.equal(facts.getPartialPerm(0, 0), 1);
+ ;});
+
+it('should correctly calculate getPartialPerm(0, 1)', () => {
+  assert.equal(facts.getPartialPerm(0, 1), 1);
+});
+
+it('should correctly calculate getPartialPerm(1, 0)', () => {
+  assert.equal(facts.getPartialPerm(1, 0), 1);
+});
+
+it('should correctly calculate getPartialPerm(4, 6)', () => {
+  assert.equal(facts.getPartialPerm(4, 6), 24);
+});
+
+it('should correctly calculate getPartialPerm(6, 4)', () => {
+  assert.equal(facts.getPartialPerm(6, 4), 360);
+});
+
+it('should correctly calculate getPartialPerm(6, 4)', () => {
+  assert.equal(facts.getPartialPerm(6, 4), 360);
+});
+it('getPartialPerm(-2, 4) should detect invalid negative number', () => {
+  assert.equal(facts.getPartialPerm(-2, 4), -1);
+});
+
+it('getPartialPerm(2, -4) should detect invalid negative number', () => {
+  assert.equal(facts.getPartialPerm(2, -4), -1);
+});
+
+it('getPartialPerm(2, 3.3) should detect invalid real number', () => {
+  assert.equal(facts.getPartialPerm(2, 2.3), -1);
+});
+
+it('getPartialPerm(.5, 3) should detect invalid real number', () => {
+  assert.equal(facts.getPartialPerm(.5, 3), -1);
+});
+
+it('getPartialPerm("six", 8) should detect invalid string input', () => {
+  assert.equal(facts.getPartialPerm("six", 8), -1);
+});
+
+it('getPartialPerm(5, "six") should detect invalid string input', () => {
+  assert.equal(facts.getPartialPerm(5, "foo"), -1);
+});
+
+it('getPartialPerm("foo") should detect invalid string input', () => {
+  assert.equal(facts.getPartialPerm("foo"), -1);
+});
+
+// tests for combinations
+
+it('should correctly calculate combination(0, 0)', () => {
+  assert.equal(facts.combination(0, 0), 1);
+ ;});
+
+it('should correctly calculate combination(0, 1)', () => {
+  assert.equal(facts.combination(0, 1), -1);
+});
+
+it('should correctly calculate getPartialPerm(1, 0)', () => {
+  assert.equal(facts.getPartialPerm(1, 0), 1);
+});
+
+it('should correctly calculate combination(6, 4)', () => {
+  assert.equal(facts.combination(6, 4), 15);
+ ;});
+
+it('should correctly calculate combination(5, 5)', () => {
+  assert.equal(facts.combination(5, 5), 1);
+ ;});
+
+it('should correctly calculate combination(4, 6)', () => {
+  assert.equal(facts.combination(4, 6), -1);
+ ;});
+
+it('should correctly calculate combination(4, 2.5)', () => {
+  assert.equal(facts.combination(4, 2.5), -1);
+ ;});
+
+it('should correctly calculate combination(4.2, 2)', () => {
+  assert.equal(facts.combination(4.2, 2), -1);
+ ;});
+
+it('should correctly calculate combination("foo", 2)', () => {
+  assert.equal(facts.combination("foo", 2), -1);
+ ;});
+
+it('should correctly calculate combination(5, "bar")', () => {
+  assert.equal(facts.combination(5, "bar"), -1);
+ ;});
+
